b257d41be59ab3df72a508f740ca1bd222a6ab16
Dlray89 <dlrayjr89@gmail.com>
2020-12-30 14:44:42 -0500

    fixed alt issues on img and resize mobile drawer logo

--- src/component/Navigation/nav_style.js
+++ src/component/Navigation/nav_style.js
@@ -43,7 +43,7 @@ export const useStyles = makeStyles((theme) => ({
       width: "40%",
     },
     [theme.breakpoints.down("xs")]: {
-      width: "43.8%",
+      width: "61%",
     },
   },
   mobileLogo: {
@@ -52,7 +52,7 @@ export const useStyles = makeStyles((theme) => ({
       width: "22em",
     },
     [theme.breakpoints.down("xs")]: {
-      width: "16em",
+      width: "12em",
     },
   },
   linksContainer: {
