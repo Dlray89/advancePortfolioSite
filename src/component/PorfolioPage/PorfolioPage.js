f7d02fd159ebe4f337a4f90ba32dfd8c099ba6aa
Dlray89 <dlrayjr89@gmail.com>
2020-12-30 12:23:55 -0500

    finished project pages and finshed responsiive design

--- src/component/PorfolioPage/PorfolioPage.js
+++ src/component/PorfolioPage/PorfolioPage.js
@@ -1,14 +1,234 @@
-import React from 'react'
-import { useStyle } from './PortfolioPageStying'
+import React, { useState, useEffect } from "react";
+import { useStyle } from "./PortfolioPageStying";
+import {
+  Card,
+  CardContent,
+  Grid,
+  CardHeader,
+  Button,
+  useTheme,
+  TextField,
+  InputAdornment,
+  useMediaQuery
+} from "@material-ui/core";
+
+import SearchIcon from "@material-ui/icons/Search";
+import { Autocomplete } from "@material-ui/lab";
+import Sauti from "../../asset/images/sauti.PNG";
+import Conway from "../../asset/images/conway.PNG";
+import Insure from "../../asset/images/insure.png";
+import Furniture_shoot from "../../asset/images/tc_shoot.PNG";
+
+const projects = [
+  {
+    id: 1,
+    name: "Sauti",
+    details:
+      "Sauti is a Mobile-based trade and market information platform to trade legally and safely across borders. Empowering East Africa's women to make a better life for themselves and their families. Sauti provides many resources to trade all across the wrold.",
+    link: "https://dlray89.github.io/SautiAfrica/",
+    githublink: "https://github.com/Dlray89/SautiAfrica",
+    tech_stack: "HTML | LESS",
+    img: Sauti,
+    alt: "Picture of Sauti Landing page",
+  },
+
+  {
+    id: 2,
+    name: "JSFurniture",
+    details:
+      "A Project creating a landing page for a furniture store called JSFurniture. Shop for the latest, most modern design in home decor all while keeping you budget intact This was a front end challenge provided buy frontend mentors.",
+    link: "https://js-furnitures.vercel.app/",
+    githublink: "https://github.com/Dlray89/JS_Furnitures",
+    tech_stack: "HTML | CSS | React | Material-UI | Javascript",
+    img: Furniture_shoot,
+    alt: "Picture of Sauti Landing page",
+  },
+  {
+    id: 3,
+    name: "Insure Insurance",
+    details:
+      "Insure Insurance Landing Page project. Insure is a life insurance company that ensure you and your loved ones are protected. User friendly process, great prices and they put there people first.",
+    link: "https://insure-insurance.vercel.app/",
+    githublink: "https://github.com/Dlray89/insure_insurance",
+    img: Insure,
+    tech_stack: "HTML | React | Material-UI",
+    alt: "Picture of Sauti Landing page",
+  },
+
+  {
+    id: 4,
+    name: "Conway | The Game Of Life",
+    details:
+      "The Game of Life was invented by Cambridge mathematician John Conway. It consists of a collection of cells which, based on a few mathematical rules, can live, die or multiply.",
+    link: "https://the-conway-game-of-life.vercel.app/",
+    githublink: "https://github.com/Dlray89/The-Conway_game-of_life",
+    tech_stack: "React | Javascript | Material-UI",
+    img: Conway,
+    alt: "Picture of Sauti Landing page",
+  },
+];
 
 const PortfolioPage = () => {
-    const classes = useStyle()
-    return(
-        <div className={classes.Portfolio}>
-            Portfolio Page
-        </div>
-    )
-}
+  const classes = useStyle();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState(projects);
+  const modileMD = useMediaQuery(theme.breakpoints.down('md'))
 
+  const handleDialog = () => {
+    setOpen(true);
+  };
 
-export default PortfolioPage
+  const closeDialog = () => {
+    setOpen(false);
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    const results = projects.filter((techProject) =>
+      techProject.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResult(results);
+  }, [search]);
+
+  return (
+    <div>
+      <div className={classes.Portfolio}></div>
+
+      <div className={classes.searchContainer}>
+       
+        <TextField
+        variant='filled'
+          className={classes.searchBar}
+          placeholder="Search Portfolio"
+          onChange={handleSearch}
+          value={search}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon fontSize="large" className={classes.searchIcon} />
+              </InputAdornment>
+            ),
+          }}
+          inputProps={{
+            style: {
+              color: "black",
+              fontSize: "1.5em",
+            },
+          }}
+        />
+      </div>
+
+      <div className={classes.cardContainer}>
+        {searchResult.map((project) => (
+          <Card variant='outlined' key={project.id}  className={classes.card}>
+            <CardHeader
+              className={classes.cardHeader}
+              title={project.name}
+              subheader={project.tech_stack}
+              titleTypographyProps={{
+                style: {
+                  fontSize: "2em",
+                },
+              }}
+              subheaderTypographyProps={{
+                style: {
+                  color: theme.palette.common.lightBrown,
+                  fontSize: "0.95em",
+                },
+              }}
+            />
+            <div className={classes.imgContainer}>
+              <img
+                className={classes.projectImages}
+                src={project.img}
+                alt={project.alt}
+              />
+            </div>
+            <CardContent className={classes.contentContainer}>
+              <Grid
+                justify="center"
+                className={classes.contentGridContainer}
+                container
+                item
+              >
+                <Grid item className={classes.contentDetails}>
+                  {project.details}
+                </Grid>
+
+                <Grid
+                  item
+                  container
+                  direction="column"
+                  justify="space-between"
+                  alignItems="center"
+                  className={classes.buttonMainContainer}
+                >
+                  <Grid item className={classes.buttonContainers}>
+                    <Button
+                      href={project.link}
+                      className={classes.buttons}
+                      variant="contained"
+                    >
+                      View Website
+                    </Button>
+                  </Grid>
+
+                  <Grid item className={classes.buttonContainers}>
+                    <Button
+                      href={project.githublink}
+                      className={classes.buttons}
+                      variant="contained"
+                    >
+                      Veiw Source
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+
+      <Grid item container className={classes.callToAction}>
+        <Grid
+          item
+          container
+          justify="center"
+          alignItems="center"
+          className={classes.callToActionLayer}
+        >
+          <Card className={classes.callToActionCard}>
+            <CardHeader title="Let get started today. " />
+            <Grid alignItems="center" justify="center" container item>
+              <Grid item className={classes.cardCTAContent}>
+                <ul>
+                  <li>Website Development</li>
+                </ul>
+
+                <ul>
+                  <li>Front-End Applications</li>
+                </ul>
+
+                <ul>
+                  <li>Responsive Design</li>
+                </ul>
+              </Grid>
+            </Grid>
+
+            <Grid container justify="center" item>
+              <Button variant="contained" className={classes.cardCTAButton}>
+                Start Service
+              </Button>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+export default PortfolioPage;
--- src/component/PorfolioPage/PortfolioPageStying.js
+++ src/component/PorfolioPage/PortfolioPageStying.js
@@ -1,8 +1,196 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles } from "@material-ui/core";
+import PortfolioImg from "../../asset/images/portImg.jpg";
+import callToactionImg from '../../asset/images/callToAction.jpg'
 
 
 export const useStyle = makeStyles((theme) => ({
-    Portfolio:{
-        border:'solid 2px red'
+  Portfolio: {
+    backgroundImage: "url(" + PortfolioImg + ")",
+    height: "25em",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "96em",
+    backgroundPositionY: "-25em",
+    backgroundPosition: "center",
+    [theme.breakpoints.down("sm")]: {
+      backgroundImage: "url(" + PortfolioImg + ")",
+      backgroundSize: "100%",
+      backgroundPosition: "center",
+      backgroundPositionY: "-0em",
+      height: "29em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      backgroundImage: "url(" + PortfolioImg + ")",
+      backgroundSize: "100%",
+      backgroundPosition: "center",
+      backgroundPositionY: "0em",
+      height: "20em",
+    },
+  },
+  searchContainer: {
+    textAlign: "center",
+    background: theme.palette.common.lightBrown,
+    padding: "2em",
+  },
+  searchBar: {
+    width: "20em",
+  },
+  searchIcon: {
+    color: theme.palette.common.black,
+    
+  },
+
+  cardContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    [theme.breakpoints.down('md')]:{
+      flexDirection:'row'
     }
-}))
+  },
+  card: {
+    margin: "2% auto",
+    background: "#242331",
+    width: "40%",
+    [theme.breakpoints.down('md')]:{
+      width:'97.5%',
+      margin:'2em auto',
+      marginLeft:'1.22em',
+
+    },
+    [theme.breakpoints.down('sm')]:{
+      width:'95%',
+      margin:'2em auto',
+    },
+    [theme.breakpoints.down('xs')]:{
+      width:'95%',
+      margin:'2em auto',
+
+    }
+  },
+  cardHeader: {
+    textAlign: "center",
+  },
+  projectImages: {
+    width: "40em",
+    [theme.breakpoints.down('md')]:{
+      width:'77.1em',
+      textAlign:'center'
+    },
+    [theme.breakpoints.down('sm')]:{
+      width:'56em'
+    },
+    [theme.breakpoints.down('xs')]:{
+      width:'36em'
+    }
+  },
+  contentContainer: {
+    width: "94%",
+    margin: "0 auto",
+    
+   
+  },
+  contentGridContainer: {
+    width: "95%",
+  },
+  contentDetails: {
+    color: theme.palette.common.lightBrown,
+    padding: "1em",
+    textAlign: "center",
+    [theme.breakpoints.down('md')]:{
+
+    },
+    [theme.breakpoints.down('sm')]:{
+      
+      marginLeft: '2em',
+      width:'100%'
+}
+  },
+
+  imgContainer: {
+    display: "flex",
+    flexDirection: "row",
+    [theme.breakpoints.down('md')]:{
+      textAlign:'center'
+    }
+  },
+  techIcons: {
+    height: "3em",
+  },
+
+  buttonMainContainer: {
+    width: "100%",
+    
+    [theme.breakpoints.down('md')]:{
+      textAlign:'center',
+    },
+    [theme.breakpoints.down('md')]:{
+      textAlign:'center',
+    },
+    [theme.breakpoints.down('xs')]:{
+      textAlign:'center',
+      marginLeft: '1.8em'
+    }
+  },
+  buttonContainers: {
+    
+    margin:'0.15em 0%',
+    [theme.breakpoints.down('md')]:{
+          marginLeft:'5em'
+    },
+    [theme.breakpoints.down('sm')]:{
+      marginLeft:'5em',
+      },
+[theme.breakpoints.down('xs')]:{
+  marginLeft: 0,
+  }
+  },
+  buttons: {
+    width: "33em",
+    [theme.breakpoints.down('md')]:{
+      width:'30em'
+    },
+    [theme.breakpoints.down('xs')]:{
+      width:'25em',
+}
+  },
+  callToAction:{
+    background:'url(' + callToactionImg +')',
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'center',
+    backgroundSize:'100em',
+    height:'60em',
+    position: 'relative',
+    marginTop:'1em',
+    marginBottom: 0,
+    [theme.breakpoints.down('md')]:{
+        backgroundSize:'75em'
+    }
+},
+callToActionLayer: {
+    backgroundColor: 'rgba(27, 20, 17, 0.9)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%'
+},
+callToActionCard:{
+    height:'18em',
+    width:'30%',
+    background: '#242331',
+    [theme.breakpoints.down('md')]:{
+        width:'70%'
+    }
+},
+cardCTAContent:{
+    color: theme.palette.common.lightBrown
+},
+cardCTAButton:{
+    background: '#558c8c',
+    color: 'white',
+    marginTop:'2em',
+    '&:hover':{
+        background: theme.palette.common.lightBrown,
+        color:'white'
+    }
+}
+}));
